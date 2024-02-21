481d79e6aaf0309be709d9e937c4a567163287db
Iresh Dissanayaka <socialexpz1@gmail.com>
2024-02-21 11:15:10 +0000

    remove unnecessary component directories

--- certbot/request-cert.sh
+++
@@ -1,6 +0,0 @@
-docker run --rm -it --name certbot -v certbot-certs:/etc/letsencrypt certbot/certbot \
-	--eff-email \
-	-m socialexpz1@gmail.com \
-	--agree-tos certonly \
-	--manual \
-	-d '*.nableintegration.ml'
